54e59a4e214434c538754b33b7b9a206f0165f09
Giovanni Leo <giovannileo595@gmail.com>
2018-10-06 16:44:00 +0200

    Aggiornamento

---
+++ Esercitazione1/Esercitazione1MNIDouble/Debug/Esercitazione1MNIDouble.d
@@ -0,0 +1,7 @@
+Esercitazione1MNIDouble.o: ../Esercitazione1MNIDouble.c \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
